817ab14a4bc144c9b94f8a8b08196cffd5f0b1be
Loai Wael <princeloai70@gmail.com>
2025-09-21 05:29:21 +0300

    FIX issues at language.ts

--- src/ts/models/language.ts
+++ src/ts/models/language.ts
@@ -1,12 +1,18 @@
-export default class Language {
-  #name
-  constructor(lang = 'en') {
-    this.#name = lang;
-    localStorage.setItem('lang', this.#name);
+export type language = 'en' | 'ar';
+
+export class Language {
+  private static _name: language
+  constructor(lang: language = 'en') {
+    Language._name = lang;
+    localStorage.setItem('lang', Language._name);
   }
 
-  changeLang(newLang) {
-    this.#name = newLang;
-    localStorage.setItem('lang', this.#name);
+  static get getCurrentLang(): language {
+    return Language._name ?? (localStorage.getItem('lang') as language)
+  }
+
+  static set changeLang(newLang: language) {
+    Language._name = newLang;
+    localStorage.setItem('lang', Language._name);
   }
 }
