beaaeecc20f0e9f880b26a99e232836f72f66724
Loai Wael <princeloai70@gmail.com>
2025-09-21 05:30:14 +0300

    UPD & FIX issues at tasks.ts

--- src/ts/models/tasks.ts
+++ src/ts/models/tasks.ts
@@ -1,51 +1,65 @@
-class Task {
-  #id
-  #name
-  #completed
-  #completedDate
-  constructor(dataObj) {
-    this.#id = crypto.randomUUID();
-    this.#name = dataObj.name;
-    this.#completed = dataObj.completed || false;
-    this.#completedDate = dataObj.completedDate;
+interface ITask {
+  id?: string;
+  name: string;
+  completed: boolean;
+  completedDate: Date | null;
+}
+
+abstract class Task {
+  protected _id: string
+  protected _name: string
+  protected _completed: boolean
+  protected _completedDate: Date | null
+  constructor(dataObj: ITask) {
+    this._id = crypto.randomUUID();
+    this._name = dataObj.name;
+    this._completed = dataObj.completed || false;
+    this._completedDate = dataObj.completedDate;
   }
 
   // read only
-  get id() {
-    return this.#id;
+  get id(): string {
+    return this._id;
   }
-  get name() {
-    return this.#name;
+  get name(): string {
+    return this._name;
   }
-  get isCompleted() {
-    return this.#completed;
+  get isCompleted(): boolean {
+    return this._completed;
   }
-  get getCompletedDate() {
-    if (this.isCompleted())
-      return this.#completedDate;
+  get getCompletedDate(): Date | null {
+    if (this.isCompleted) {
+      return this._completedDate;
+    } else {
+      return null
+    }
   }
 
   // write only
-  set name(newName) {
-    this.#name = newName;
+  set name(newName: string) {
+    this._name = newName;
   }
-  set complete(complete = true) {
-    this.#completed = complete;
-    this.#completedDate = new Date();
+  set complete(complete: boolean) {
+    this._completed = complete;
+    this._completedDate = new Date();
   }
 }
 
+export interface IDailyTask extends ITask {
+  time: string;
+}
+
 export class DailyTask extends Task {
-  #time
-  constructor(dataObj) {
+  private _time: string
+  constructor(dataObj: IDailyTask) {
     super(dataObj);
-    this.#time = dataObj.time;
+    this._time = dataObj.time;
   }
 
-  get time() {
-    return this.#time;
+  get time(): string {
+    return this._time;
   }
   set time(newTime) {
-    this.#time = newTime;
+    this._time = newTime;
   }
 }
