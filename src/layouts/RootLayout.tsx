7e0a008be5f369a94eec5c5ab8ed114d9742eac9
Loai Wael <loaiwael.dev@gmail.com>
2026-02-14 01:38:25 +0200

    FIX Layout issues

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,8 @@
-import SideBar from "@/components/SideBar"
 import { Outlet } from "react-router-dom"
 
 const RootLayout = () => {
   return (
     <>
-      <SideBar />
       <Outlet />
     </>
   )
--- src/router/ProtectedRoutes.tsx
+++ src/router/ProtectedRoutes.tsx
@@ -1,12 +1,18 @@
+import SideBar from "@/components/SideBar";
 import { useUser } from "@/contexts/UserContext"
 import SetupPage from "@/pages/SetupPage";
 import { Outlet } from "react-router-dom";
 
 
 const ProtectedRoutes = () => {
-  const { user } = useUser();
+  const { userState } = useUser();
 
-  if (user.isAuth) return (<Outlet />);
+  if (userState.isAuth) return (
+    <>
+      <SideBar />
+      <Outlet />
+    </>
+  );
   else return (<SetupPage />)
 }
 
